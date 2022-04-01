67aaf0786fe88f6fe92d961a76d90302d80ec43a
Geekanunnaki <70728959+Geekanunnaki@users.noreply.github.com>
2022-04-01 16:12:07 -0300

    Add files via upload

---
+++ Filmes  Geeks  .py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+filmes_1 = (" Resident Evil")
+filmes_2 = ("Silent Hill")
+filmes_3 = ("A Cura" )
+
+filmes_0 = (filmes_1)
+print (filmes_1)
+
+
+# In[4]:
+
+
+filmes_0 = ("filmes_2")
+print (filmes_2)
+
+
+# In[5]:
+
+
+filme_0 = ("filmes_3")
+print  (filmes_3)
+
+
+# In[6]:
+
+
+old_filmes = ("Alien o 8° Passageiro") 
+print (old_filmes)
+
+
+# In[7]:
+
+
+old_1_filmes = ("Enigma do Horizonte ")
+print (old_1_filmes)
+
+
+# In[8]:
+
+
+old_2_filmes = ( "Helraiser ")
+print ("old_2_filmes ")
+
+
+# In[9]:
+
+
+old_3_filmes = ("O lado escuro da lua ")
+print (old_3_filmes)
+
+
+# In[10]:
+
+
+ficção_filmes = ("Matrix, Pandorium , Jonny Mnemonic, Stars Wars")
+print (ficção_filmes)
+
+
+# In[11]:
+
+
+ficção_1 = ("Matrix ")
+ficção_2 = ("Pandorium")
+ficção_3 = ("Jonny Mnemonic")
+ficção_4 = ("Stars Wars")
+
+ficção_0 = ("Matrix")
+print (ficção_0)
+           
+
+
+# In[12]:
+
+
+ficção_0 = ("Pandorium")
+print (ficção_0)
+
+
+# In[13]:
+
+
+ficção_0 = ("Jonny Mnemonic")
+print (ficção_0)
+
+
+# In[5]:
+
+
+ficção_0 = ("Stars Wars")
+print (ficção_0)
+
+
+# In[6]:
+
+
+Marvel_movies = ("Avengers, Guerra Infinita, Capitão America o Primeiro Vingador" )
+print (Marvel_movies)
+
+
+# In[ ]:
+
+
+
+
