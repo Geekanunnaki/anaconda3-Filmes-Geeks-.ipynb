bf63fc7599b17b88d7b020dd58993a7e876cc1d1
Geekanunnaki <70728959+Geekanunnaki@users.noreply.github.com>
2022-04-01 16:10:18 -0300

    Add files via upload

---
+++ Cadastro de Clientes.sql
@@ -0,0 +1,14 @@
+create Database  
+Projeto Cadastro 
+Cadastro de Funcionários  
+Cadastro de clientes 
+nomes : Andre 
+sobrenome: Arjona Talassi
+Idade : 46 
+Cargo : Desenvolvedor Back end
+Companhia : PTC Groups 
+CPF: 263546728-57 
+Escolaridade : Superior Cursando
+Curso : Engenharia de Software
+modalidade : EAD
+  
